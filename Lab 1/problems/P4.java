2e3452459c187d1ddb41412c77f7bf7dd28ab1f0
xianan2 <65153699+xianan2@users.noreply.github.com>
2020-08-31 12:04:38 +0800

    Create P4.java

---
+++ Lab 1/problems/P4.java
@@ -0,0 +1,35 @@
+package problems;
+
+import java.util.Scanner;
+
+public class P4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int h = sc.nextInt();
+		
+		if (h < 1)
+			System.out.println("Error Input!!");
+		else {
+			for(int i = 1; i <= h; i++) {
+				for(int j = 1; j <= i; j++) {
+					if (i % 2 == 1) {
+						if (j % 2 == 1)
+							System.out.print("AA");
+						else
+							System.out.print("BB");
+					}
+					else {
+						if (j % 2 == 1)
+							System.out.print("BB");
+						else
+							System.out.print("AA");
+					}
+				}
+				System.out.println("");
+			}
+		}
+	}
+
+}
